7b833f5c8f8041ae14cbcae558402c05b31edb28
Bruno Rangel <bruno14.rangel@gmail.com>
2018-11-12 13:00:34 -0600

    Drag Build Up
    Started drag build up function

---
+++ Aero 261 functions/Drag_Build_Up.m
@@ -0,0 +1,18 @@
+function [] = Drag_Build_Up(Re,inputArg2)
+
+Cf = 1.328/Re;
+
+SRefWing = ;
+SWetWing = ;
+
+
+CD_Wing = (Cf*FF*1*SWetWing
+
+CD_Fuselage = 
+
+CD_Empennage = 
+
+CD_Nacelles = 
+
+end
+
