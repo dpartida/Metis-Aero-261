2e7a952f7f4fb873c2e34b5961cc3d8fac571c12
GrantKelly99 <gtkelly@iastate.edu>
2018-11-13 18:40:03 -0600

    updates to FT2

--- Aero 261 functions/maxrangecalculator.m
+++ Aero 261 functions/maxrangecalculator.m
@@ -1,6 +1,6 @@
 function [d_BR] = maxrangecalculator(v_BR,t)
 %This function calculates max range distance
 %   relies on v_BR(velocity best range) and t(time to destination)
-d_BR = (v_BR/(
+d_BR = ((v_BR/(TSFC*(1/(((sqrt3)/2)*Em))))*ln(1/
 end
 
