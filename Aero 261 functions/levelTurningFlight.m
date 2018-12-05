4fa5baa699b2f280d74e02c44f1c3e6f41ef662d
adam orgler <aorgler@iastate.edu>
2018-12-05 12:24:58 -0600

    small change

--- Aero 261 functions/levelTurningFlight.m
+++ Aero 261 functions/levelTurningFlight.m
@@ -1,12 +1,12 @@
-function [maxLoadingFactor,maxBankAngleVelocity,yawRate,turnRadius,v_turning] = levelTurningFlight(Em,Th,W,S,Cd0,K,rho,g)
+function [maxLoadingFactor,maxBankAngleVelocity,yawRate,turnRadius] = levelTurningFlight(Em,Th,W,S,Cd0,K,rho,g)
 maxLoadingFactor = Em * (Th / W)
 maxBankAngle = acos(1/maxLoadingFactor)
 v_positive = sqrt(((T / W) * (W / S) + (W / S) * sqrt((T / W)^2 - 4 * K * Cd0 * n^2)) / ( rho * Cd0 ))
 v_negative = sqrt(((T / W) * (W / S) - (W / S) * sqrt((T / W)^2 - 4 * K * Cd0 * n^2)) / ( rho * Cd0 ))
 if v_positive >= v_negative
-    v_turning = v_positive
+    maxBankAngleVelocity = v_positive
 elseif v_negative >= v_positive
-    v_turning = v_negative
+    maxBankAngleVelocity = v_negative
 else
     print("WTF?!")
     maxBankAngleVelocity = v_positive
