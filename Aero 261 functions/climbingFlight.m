74afea81a6609992dcd111f0e8e77077571dd4b9
adam orgler <aorgler@iastate.edu>
2018-12-11 15:34:23 -0600

    syntax changes

--- Aero 261 functions/climbingFlight.m
+++ Aero 261 functions/climbingFlight.m
@@ -1,4 +1,4 @@
-function [v_steep, v_fast, angle_steep, angle_fast, roc_steep, roc_fast] climbingFlight = [T,W,S,K,Cd0,p]
+function [v_steep, v_fast, angle_steep, angle_fast, roc_steep, roc_fast] = climbingFlight(T,W,S,K,Cd0,p)
 gamma = 1 + sqrt(1 + ((12 * K * Cd0)/(T / w)^2))
 angle_steep = (T/W) - 2*sqrt(k * Cd0)
 angle_fast = ((T/W) * (1- (gamma/6))) - (6 * K * Cd0)/(gamma * (T/W))
